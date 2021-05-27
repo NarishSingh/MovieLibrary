807a5ceac150147d7942669c26a50ec94ed518e3
Narish Singh <62305841+NarishSingh@users.noreply.github.com>
2021-05-27 16:01:41 -0400

    Interface for Movie repo started

--- MovieLibrary/MovieLibrary.Data/IMovieRepo.cs
+++ MovieLibrary/MovieLibrary.Data/IMovieRepo.cs
@@ -1,10 +1,35 @@
-﻿namespace MovieLibrary.Data
+﻿using MovieLibrary.Models.Db;
+
+namespace MovieLibrary.Data
 {
     /// <summary>
     /// Will handle CRUD to the database
     /// </summary>
     public interface IMovieRepo
     {
-        
+        /// <summary>
+        /// When a movie record receives >0 likes or dislikes, persist to db
+        /// </summary>
+        /// <param name="movie">well form Movie obj</param>
+        void CreateMovie(Movie movie);
+
+        /// <summary>
+        /// Read a movie obj from db
+        /// </summary>
+        /// <param name="movieId">int for a valid movie id</param>
+        /// <returns>Movie obj corresponding to that id</returns>
+        Movie ReadMovieById(int movieId);
+
+        /// <summary>
+        /// If a movie gets a like or dislike, update record in db
+        /// </summary>
+        /// <param name="update">well formed Movie obj to be updated</param>
+        void UpdateMovie(Movie update);
+
+        /// <summary>
+        /// When a movie's likes and dislikes both reach 0, delete from db
+        /// </summary>
+        /// <param name="movieId">valid id for a movie record in db</param>
+        void DeleteMovie(int movieId);
     }
 }
---
+++ MovieLibrary/MovieLibrary.UI/Models/MovieVM.cs
@@ -0,0 +1,11 @@
+﻿
+namespace MovieLibrary.UI.Models
+{
+    /// <summary>
+    /// Deserialize JSON's into these view movels
+    /// </summary>
+    public class MovieVM
+    {
+        
+    }
+}
