e31d8491f92bd2e4adefdd6ccdec866257f5fabe
Narish Singh <62305841+NarishSingh@users.noreply.github.com>
2021-05-31 21:30:56 -0400

    Formatting

--- MovieLibrary/MovieLibrary.Data/MovieApiImpl.cs
+++ MovieLibrary/MovieLibrary.Data/MovieApiImpl.cs
@@ -9,7 +9,7 @@ namespace MovieLibrary.Data
 {
     public class MovieApiImpl : IMovieApi
     {
-        private static readonly HttpClient _client = new HttpClient();
+        private static readonly HttpClient Client = new HttpClient();
         private const string ApiKey = "d5150b3ec8f4f8de295a897de8623169";
         
         public async Task<IEnumerable<MovieShortItem>> SearchByTitle(string title)
@@ -25,11 +25,12 @@ namespace MovieLibrary.Data
                         $"https://api.themoviedb.org/3/search/movie?api_key={ApiKey}&language=en-US&query={title}&page=1&include_adult=false")
             };
 
-            //send request, await, deserialize to root JSON array container obj
-            using (HttpResponseMessage response = await _client.SendAsync(request))
+            //send request, await
+            using (HttpResponseMessage response = await Client.SendAsync(request))
             {
                 response.EnsureSuccessStatusCode();
 
+                //convert JSON to string and deserialize to root JSON array container obj
                 string body = await response.Content.ReadAsStringAsync();
                 movieList = JsonConvert.DeserializeObject<SearchResults>(body);
             }
