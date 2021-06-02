8f0647fa09a097b723015ed94833241a26ad7cd8
Narish Singh <62305841+NarishSingh@users.noreply.github.com>
2021-06-02 15:03:48 -0400

    Field to store posters for movie

--- MovieLibrary/MovieLibrary.Models/API/MovieDetailedItem.cs
+++ MovieLibrary/MovieLibrary.Models/API/MovieDetailedItem.cs
@@ -14,5 +14,6 @@ namespace MovieLibrary.Models.API
         public IEnumerable<string> Directors { get; set; } //Not a JSON property, will need a separate API call
         [JsonProperty("release_date")] public DateTime ReleaseDate { get; set; }
         [JsonProperty("overview")] public string Description { get; set; }
+        [JsonProperty("poster_path")] public string PosterPath { get; set; } //https://image.tmdb.org/t/p/w + width int + / + this prop for poster image
     }
 }
