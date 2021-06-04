e05933eb273c3ca0d128393ce4c2943bf5876f0d
Narish Singh <62305841+NarishSingh@users.noreply.github.com>
2021-06-04 15:25:44 -0400

    Starting front end views

--- MovieLibrary/MovieLibrary.UI/Controllers/HomeController.cs
+++ MovieLibrary/MovieLibrary.UI/Controllers/HomeController.cs
@@ -1,28 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Collections.Generic;
 using System.Web.Mvc;
+using MovieLibrary.Models.API;
+using MovieLibrary.Service;
 
 namespace MovieLibrary.UI.Controllers
 {
     public class HomeController : Controller
     {
+        private IService _service = new ServiceImpl();
+        
+        [HttpGet]
         public ActionResult Index()
         {
-            return View();
-        }
-
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-            return View();
+            IEnumerable<MovieShortItem> model = _service.SearchNowPlaying();
+            return View(model);
         }
     }
 }
