416e2922bd7e3d9132a924dc4fafae91d2780abc
Narish Singh <62305841+NarishSingh@users.noreply.github.com>
2021-06-09 20:25:09 -0400

    Removed modernizr, not using it

--- MovieLibrary/MovieLibrary.UI/App_Start/BundleConfig.cs
+++ MovieLibrary/MovieLibrary.UI/App_Start/BundleConfig.cs
@@ -13,9 +13,6 @@ namespace MovieLibrary.UI
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                 "~/Scripts/jquery.validate*"));
 
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                "~/Scripts/modernizr-*"));
-
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                 "~/Scripts/bootstrap.js"));
 
