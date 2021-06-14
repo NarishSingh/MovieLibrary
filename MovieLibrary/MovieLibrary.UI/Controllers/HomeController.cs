1ffedc3524455ec8e7ffd3b923e72697a4b47456
Narish Singh <62305841+NarishSingh@users.noreply.github.com>
2021-06-14 12:47:54 -0400

    Carousel finished

--- MovieLibrary/MovieLibrary.UI/Controllers/HomeController.cs
+++ MovieLibrary/MovieLibrary.UI/Controllers/HomeController.cs
@@ -1,11 +1,7 @@
-﻿using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using System.Web.Mvc;
-using MovieLibrary.Models.API;
 using MovieLibrary.Models.Service;
 using MovieLibrary.Service;
-using MovieLibrary.UI.Models;
 
 namespace MovieLibrary.UI.Controllers
 {
