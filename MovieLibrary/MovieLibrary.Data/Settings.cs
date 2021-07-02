7e2d13b25f6fe47bc718605513e48c657d201bb7
Narish Singh <62305841+NarishSingh@users.noreply.github.com>
2021-07-02 15:00:40 -0400

    App up and running with production db, all tests for solution passing with test db

--- MovieLibrary/MovieLibrary.Data/Settings.cs
+++ MovieLibrary/MovieLibrary.Data/Settings.cs
@@ -7,16 +7,14 @@ namespace MovieLibrary.Data
         private static string _connString;
         
         /// <summary>
-        /// Get the connection string from config
+        /// Get the connection string from project's respective .config file
         /// </summary>
         /// <returns>string for connection string to db</returns>
         public static string GetConnString()
         {
             if (string.IsNullOrEmpty(_connString))
             {
-                _connString = ConfigurationManager
-                    .ConnectionStrings["MovieLibrary"]
-                    .ConnectionString;
+                _connString = ConfigurationManager.ConnectionStrings["MovieLibrary"].ConnectionString;
             }
 
             return _connString;
